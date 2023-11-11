1b365f4551f4fbc80f1faf0234e4a3266a19331f
nrusnac <nrusnac.nr@gmail.com>
2023-11-11 09:21:33 +0100

    Implemented the optimal function value and tested it against the naive option

--- function.py
+++ function.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import itertools
+import math
 
 class Gate(Enum):
     AND = "and"
@@ -11,10 +12,17 @@ class BoolFunction:
         self.children = children
         self.gate = gate
         self.gateOperator = None
-        if (self.gate == Gate.AND):
+        self.parent = None
+        self.trueChildren = 0
+        
+        if self.gate == Gate.AND:
             self.gateOperator = all
-        elif (self.gate == Gate.OR):
+        elif self.gate == Gate.OR:
             self.gateOperator = any
+        
+        if self.children:
+            for child in children:
+                child.parent = self
 
     def getVars(self):
         vars = []
@@ -34,9 +42,25 @@ class BoolFunction:
                 return self.value
         if (self.gate == None):
             return self.value
+        self.value = self.gateOperator(childrenValues)
         return self.gateOperator(childrenValues)
-
-def exampleFunction():
+    
+    def propagateChange(self):
+        if self.parent == None:
+            return 
+        
+        if self.value:
+            self.parent.trueChildren += 1
+        else:
+            self.parent.trueChildren -= 1
+            
+        prevVal = self.parent.value and 1
+        self.parent.value = self.parent.trueChildren == len(self.parent.children) if self.parent.gate == Gate.AND else self.parent.trueChildren > 0 
+        if self.parent.value == prevVal:
+            return
+        return self.parent.propagateChange()
+        
+def functionExample()
     v1 = BoolFunction(1)
     v2 = BoolFunction(1)
     v3 = BoolFunction(0)
@@ -54,7 +78,7 @@ def exampleFunction():
     l3_1 = BoolFunction(None, [l2_1,l2_2], Gate.AND)
     return l3_1
 
-rootNode = exampleFunction()
+rootNode = functionExample()
 
 def generate_binary_combinations(n):
     return itertools.product([0, 1], repeat=n)
@@ -64,17 +88,49 @@ def generate_binary_combinations(n):
 def evaluateFunctionNaively(rootNode):
     vars = rootNode.getVars()
     data = list(generate_binary_combinations(len(vars)))
-    functionEvaluation = {}
+    functionEnumeration = {}
     
     for dataPoint in data:
         for i,var in enumerate(vars):
-            print(i, var)
             var.value = dataPoint[i]
-        functionEvaluation[dataPoint] = var.value
+        functionEnumeration[''.join(map(str, dataPoint))] = rootNode.evaluate()
         
-    print(len(functionEvaluation))
+    return functionEnumeration
 
-def evaluateFunctionOptimally():
-    pass
+def generate_gray_code_with_changed_bit_indices(n):
+    result = []
+    previous_gray = 0  # Initialize with the first Gray code value
+    for i in range(0, 2**n):
+        current_gray = i ^ (i >> 1)
+        if i == 0:
+            changed_bit_index = None  # No bit changed for the first element
+        else:
+            changed_bit = previous_gray ^ current_gray
+            changed_bit_index = (changed_bit & -changed_bit).bit_length() - 1
+        result.append((format(current_gray, f'0{n}b'), None if changed_bit_index == None else n - changed_bit_index - 1))
+        previous_gray = current_gray  # Update previous_gray for the next iteration
+    return result
 
-evaluateFunctionNaively(rootNode)
+def evaluateFunctionOptimally(rootNode):
+    vars = rootNode.getVars()
+    data = generate_gray_code_with_changed_bit_indices(len(vars))
+    functionEnumeration = {}
+    for dataPoint,changedBit in data:
+        if changedBit == None:
+            for var in vars:
+                var.value = 0
+                functionEnumeration[dataPoint] = 0
+                rootNode.evaluate()
+        else:
+            vars[changedBit].value = not vars[changedBit].value
+            vars[changedBit].propagateChange()
+            functionEnumeration[dataPoint] = rootNode.value
+                
+    return functionEnumeration
+
+data1 = evaluateFunctionNaively(rootNode)
+data2 = evaluateFunctionOptimally(rootNode)
+
+for key,val in data1.items():
+    print(data1[key], data2[key], key)
+    assert(data1[key] == data2[key])
